0b07e691bb9bd2ebc630dc0c0bd370d27a8fb7e2
Aweptimum <40273116+Aweptimum@users.noreply.github.com>
2021-09-02 21:28:13 -0400

    Create Rectangle.lua
    AABB-style constructor, but still a polygon at heart.
    
    Does not have a simple `get_bbox` method because of possible rotation.

---
+++ shapes/Rectangle.lua
@@ -0,0 +1,38 @@
+local Vec = require "Slap.DeWallua.vector-light"
+local cos, sin = math.cos, math.sin
+local polygon = require 'Slap.shapes.ConvexPolygon'
+
+local Rect = {
+    vertices 		= {},              -- list of {x,y} coords
+    convex      	= true,             -- boolean
+    centroid   		= {x = 0, y = 0},	-- {x, y} coordinate pair
+    radius			= 0,				-- radius of circumscribed circle
+    area			= 0					-- absolute/unsigned area of polygon
+}
+-- Set metatable so we can override polygon methods
+--Rect.__index = Rect
+Rect = polygon:extend()
+
+Rect.name = 'rect'
+function Rect:new(x_pos, y_pos, dx, dy, angle_rads)
+	print('constructing rect')
+	if not ( dx and dy ) then return false end
+	local x_offset, y_offset = x_pos or 0, y_pos or 0
+    self.dx, self.dy = dx, dy
+	self.angle = angle_rads or 0
+	self.vertices = {
+		{x = x_offset					, y = y_offset					},
+		{x = x_offset + dx*cos(angle)	, y = y_offset					},
+		{x = x_offset + dx*cos(angle)	, y = y_offset + dy*sin(angle)	},
+		{x = x_offset					, y = y_offset + dy*sin(angle)	}
+	}
+	self.centroid  	= {x = x_offset+dx/2, y = y_offset+dy/2}
+	self.area 		= dx*dy
+	self.radius		= Vec.len(dx, dy)
+end
+
+function Rect:unpack()
+	return self.centroid.x, self.centroid.y, self.dx, self.dy, self.angle
+end
+
+return Rect
