2fce13b5af0f71337d864fa5bc2f48a4ea1cd803
Aweptimum <40273116+Aweptimum@users.noreply.github.com>
2021-09-02 21:36:17 -0400

    Create shapes.lua
    Loads all the shapes in the shapes/ folder into a table and returns it. Does a little idiot checking.
    
    **The key of each shape in the table is the name of each required filed.

---
+++ shapes.lua
@@ -0,0 +1,35 @@
+local scandir 	= _Require_relative(..., 'fileloader')
+local Shape     = require 'Slap.shapes.Shape'
+-- Two kinds of shapes:
+-- circles and polygons
+
+local Shapes = {}
+
+function Shapes:Create_Definition(name, object)
+    print('Creating definition for:' .. name)
+    --Check that the object implements new
+    assert(object.new, "\tnew is null for shape: ".. name)
+
+    self[name] = object
+end
+
+local function load_shapes(cwd)
+    local shape_files = scandir('shapes')
+    tprint(shape_files)
+    for _, filename in ipairs(shape_files) do
+        local name = filename:sub(1,filename:len()-4)
+        local s = _Require_relative(cwd, 'shapes.'..name)
+        if type(s) == 'table' and s:implements(Shape) then
+            -- Add to shapes
+            Shapes:Create_Definition(name, s)
+        else
+            Shapes[name] = false
+        end
+    end
+end
+
+load_shapes(...)
+
+-- Shapes: edge, aabb, ellipse, regular poly, convex poly, concave poly
+
+return Shapes
