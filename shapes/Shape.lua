a576ad9decd003b1e9de6cc1155e6a38b103ffd5
Aweptimum <40273116+Aweptimum@users.noreply.github.com>
2021-09-02 21:14:44 -0400

    Create Shape.lua
    Sort-of abstract base class that other shapes can inherit from. Implements a few methods other shapes will invariably need, including a generic copy. Requires other shapes implement their own behavior.
    
    For instance `:new` should be different for a circle and a polygon. `:unpack`, however, should always return the arguments of a shape in the order needed to construct the same shape. Then we can do `self( self:unpack() )` with no fear.

---
+++ shapes/Shape.lua
@@ -0,0 +1,64 @@
+-- "Abstract" base object, provides some generic methods.
+local Object = require 'Slap.classic.classic'
+
+local Shape = Object:extend()
+
+function Shape:get_area()
+    return self.area
+end
+
+function Shape:get_centroid()
+    return self.centroid
+end
+
+function Shape:get_area_centroid()
+    return self.area, self.centroid
+end
+
+function Shape:get_bbox()
+end
+
+function Shape:get_radius()
+end
+
+function Shape:project()
+end
+
+function Shape:unpack()
+end
+
+function Shape:translate()
+end
+
+function Shape:rotate()
+end
+
+function Shape:scale()
+end
+
+function Shape:copy(x, y, angle_rads)
+    local copy = self( self:unpack() )
+	-- if origin specified, then translate_polygon
+	if x or y then
+		local dx = x and x - self.centroid.x or 0 -- amount to translate in x if x specified
+    	local dy = y and y - self.centroid.y or 0 -- amount to translate in y if y specified
+		copy:_translate(dx, dy)
+	end
+	-- If rotation specified, then rotate_polygon
+	if angle_rads then
+		copy:_rotate(angle_rads)
+    end
+	-- Return copy
+	return copy
+end
+
+function Shape:calc_area()
+end
+
+function Shape:calc_centroid()
+end
+
+function Shape:calc_area_centroid()
+end
+
+return Shape
