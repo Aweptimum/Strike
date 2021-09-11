aa1ec7435a7a71510a0dd2c34d83ae5663bd3506
Aweptimum <40273116+Aweptimum@users.noreply.github.com>
2021-09-11 18:09:58 -0400

    Create Capsule.lua
    This was surprisingly easy to throw together and get working

---
+++ colliders/Capsule.lua
@@ -0,0 +1,26 @@
+local pop = table.remove
+local cos, sin = math.cos, math.sin
+local Collider  = require 'Strike.colliders.Collider'
+local Circle    = require 'Strike.shapes.Circle'
+local Rectangle = require 'Strike.shapes.Rectangle'
+
+local Capsule = Collider:extend()
+
+-- A Capsule is a rectangle + 2 circles
+function Capsule:new(x, y, dx, dy, angle_rads)
+    self.shapes = {}
+    self.centroid = {x=0,y=0}
+    self.radius = 0
+    self.angle = angle_rads or 0
+    local hx, hy = dx/2, dy/2
+    self:add(
+        Circle(x, y-hy, hx),
+        Circle(x, y+hy, hx),
+        Rectangle(x,y,dx,dy)
+    )
+    self:calc_area_centroid()
+    self:calc_radius()
+    self:rotate(self.angle)
+end
+
+return Capsule
