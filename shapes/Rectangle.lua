1c71d2fcd07c6f7aa220c64698a96b2314bc1294
Aweptimum <40273116+Aweptimum@users.noreply.github.com>
2021-09-11 17:57:27 -0400

    Delete struct, re-do construction
    Originally the rectangle was constructed wrt to the bottom-left point, but I think it's better that x/y is the centroid. More consistent with all the other shapes, plays nicer with collider definitions dependent on it.

--- shapes/Rectangle.lua
+++ shapes/Rectangle.lua
@@ -1,34 +1,25 @@
 local Vec = require "Strike.DeWallua.vector-light"
-local cos, sin = math.cos, math.sin
 local Polygon = require 'Strike.shapes.ConvexPolygon'
 
-local Rect = {
-    vertices 		= {},              -- list of {x,y} coords
-    convex      	= true,             -- boolean
-    centroid   		= {x = 0, y = 0},	-- {x, y} coordinate pair
-    radius			= 0,				-- radius of circumscribed circle
-    area			= 0					-- absolute/unsigned area of polygon
-}
--- Set metatable so we can override polygon methods
---Rect.__index = Rect
 Rect = Polygon:extend()
 
 Rect.name = 'rect'
+
 function Rect:new(x_pos, y_pos, dx, dy, angle_rads)
-	print('constructing rect')
 	if not ( dx and dy ) then return false end
 	local x_offset, y_offset = x_pos or 0, y_pos or 0
     self.dx, self.dy = dx, dy
 	self.angle = angle_rads or 0
+	local hx, hy = dx/2, dy/2 -- halfsize
 	self.vertices = {
-		{x = x_offset		, y = y_offset     },
-		{x = x_offset + dx	, y = y_offset     },
-		{x = x_offset + dx	, y = y_offset + dy},
-		{x = x_offset		, y = y_offset + dy}
+		{x = x_offset - hx, y = y_offset - hy},
+		{x = x_offset + hx, y = y_offset - hy},
+		{x = x_offset + hx, y = y_offset + hy},
+		{x = x_offset - hx, y = y_offset + hy}
 	}
-	self.centroid  	= {x = x_offset+dx/2, y = y_offset+dy/2}
+	self.centroid  	= {x = x_offset, y = y_offset}
 	self.area 		= dx*dy
-	self.radius		= Vec.len(dx, dy)/2
+	self.radius		= Vec.len(hx, hy)
 	self:rotate(self.angle)
 end
 
