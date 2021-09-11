cf5466b212049f51ebfd5341a97ab9ff8b82efcf
Aweptimum <40273116+Aweptimum@users.noreply.github.com>
2021-09-11 17:52:41 -0400

    Re-ordered properties, explicitly declare radius

--- colliders/Concave.lua
+++ colliders/Concave.lua
@@ -17,14 +17,15 @@ function Concave:new(...)
 	if ... and type(...) == "table" then return self:new(unpack(...)) end
     local vertices = to_vertices({}, ...)
 	assert(#vertices >= 3, "Need at least 3 non collinear points to build polygon (got "..#vertices..")")
-    -- Triangulation!
     self.shapes = {}
+    self.centroid = {x=0, y=0}
+    self.radius = 0
+    -- Triangulation!
     local triangles = Dewall.constrained(vertices)
 	-- Convert triangles to polygons and add to self.shapes
 	for i = #triangles, 1, -1 do
 		self:add(Convex(unpack(pop(triangles))))
 	end
-    self.centroid = {x=0, y=0}
     -- Need to merge triangles into the largest concave polygons possible
     -- Then, for each polygon, calc area/centroid
     -- Figure out how to store several sub-polygons in a consistent way
