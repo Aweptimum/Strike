3d990c515a9d21cac4310505b5c086778de48b5a
Aweptimum <40273116+Aweptimum@users.noreply.github.com>
2021-10-30 14:47:34 -0400

    Annotated RegPoly

--- shapes/RegularPolygon.lua
+++ shapes/RegularPolygon.lua
@@ -1,29 +1,35 @@
-local pi, cos, sin, tan = math.pi, math.cos, math.sin, math.tan
-
-local Vec = Libs['vector-light']
 local Polygon = _Require_relative(..., 'ConvexPolygon')
 
+local pi, cos, sin, tan = math.pi, math.cos, math.sin, math.tan
+
+---@class RegularPolygon : ConvexPolygon
 local RegularPolygon = Polygon:extend()
 
+---Calculate area using regular area formula
+---@return number area
 function RegularPolygon:calcArea()
     local n, r = self.n, self.radius*cos(pi/self.n)
     self.area = n*r*r*tan(pi/n)
+    return self.area
 end
 
-function RegularPolygon:new(x_pos, y_pos, n, radius, angle_rads)
+---Regular Polygon ctor
+---@param x number x position
+---@param y number y position
+---@param n number of sides
+---@param radius number radius of circumscribed circle
+---@param angle number radian offset
+function RegularPolygon:new(x, y, n, radius, angle)
     -- Initialize our polygon's origin and rotation
     n = n or 3
-    self.angle = angle_rads or 0
+    self.angle = angle or 0
     -- Initalize our dummy point vars to put into the vertices list
-    local x, y
     local vertices = {}
 
-    -- Print easter-egg warning
-    --if n == 1000 then print("OH NO, NOT A CHILIAGON! YOU MANIAC! HOW COULD YOU!?\n") end
     -- Calculate the points
     for i = n, 1, -1 do -- i = 1, n calculates vertices in clockwise order, so go backwards
-        x = ( sin( i / n * 2 * pi - self.angle) * radius) + x_pos
-        y = ( cos( i / n * 2 * pi - self.angle) * radius) + y_pos
+        x = ( sin( i / n * 2 * pi - self.angle) * radius) + x
+        y = ( cos( i / n * 2 * pi - self.angle) * radius) + y
         vertices[#vertices+1] = {x = x, y = y}
     end
 
@@ -31,11 +37,17 @@ function RegularPolygon:new(x_pos, y_pos, n, radius, angle_rads)
     self.n, self.radius = n, radius
     self.vertices 		= vertices     	    -- list of {x,y} coords
     self.convex      	= true      		-- boolean
-    self.centroid       = {x = x_pos, y = y_pos}	-- {x, y} coordinate pair
+    self.centroid       = {x = x, y = y}	-- {x, y} coordinate pair
     -- Calculate the area of our polygon.
     self:calcArea()
 end
 
+---Return ctor args
+---@return number x
+---@return number y
+---@return number n
+---@return number radius
+---@return number angle
 function RegularPolygon:unpack()
     return self.centroid.x, self.centroid.y, self.n, self.radius, self.angle
 end
